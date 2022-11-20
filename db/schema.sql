df494397ce348b345d3dfc1870ae835d49a24b2e
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2022-11-20 16:09:19 -0700

    require dependencies

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+
+USE employees;
+
+CREATE TABLE emloyee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+);
+
+
