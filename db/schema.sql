88871402a5bf7c301bfc97744867838593f96375
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2022-11-20 17:57:33 -0700

    seeds

--- db/schema.sql
+++ db/schema.sql
@@ -18,10 +18,38 @@ CREATE TABLE department (
 
 CREATE TABLE role (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) UNIQUE NOT NULL,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
+INSERT INTO department
+  (name)
+VALUES 
+  ('Sales'),
+  ('Engineering'),
+  ('Finance'),
+  ('Legal');
+INSERT INTO role
+  (title, salary, department_id)
+VALUES
+  ('Sales Lead', 100000, 1),
+  ('Salesperson', 80000, 1),
+  ('Lead Engineer', 150000, 2),
+  ('Software Engineer', 120000, 2),
+  ('Account Manager', 160000, 3),
+  ('Accountant', 125000, 3),
+  ('Legal Team Lead', 250000, 4),
+  ('Lawyer', 190000, 4);
+INSERT INTO employee
+  (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Virginia', 'Vermillion', 1, NULL),
+  ('Ronald', 'Kelly', 1, NULL),
+  ('Stacy', 'Wilke', 2, NULL),
+  ('Edgar', 'Rogers', 2, NULL),
+  ('Andrew', 'Monroe', 3, NULL),
+  ('Frank', 'Townsend', 3, NULL),
+  ('Jill', 'Polk', 4, NULL),
+  ('Ruth', 'Willimas', 4, NULL);
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 require('console.table');
 
+const connection = require('./config/connection')
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -12,9 +14,62 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-const questions = [
+const promptMessages = {
+  viewAllEmployees: "View All Employees",
+  viewByDepartment: "View All Employees By Department",
+  viewByManager: "View All Employees By Manager",
+  addEmployee: "Add An Employee",
+  removeEmployee: "Remove An Employee",
+  updateRole: "Update Employee Role",
+  updateEmployeeManager: "Update Employee Manager",
+  viewAllRoles: "View All Roles",
+  exit: "Exit"
+};
 
-]
+function prompt() {
+  inquirer  
+    .prompt({
+      name: 'action',
+      type: 'list',
+      message: "What would you like to do?",
+        choices: [
+          promptMessages.viewAllEmployees,
+          promptMessages.viewByDepartment,
+          promptMessages.viewByManager,
+          promptMessages.viewAllRoles,
+          promptMessages.addEmployee,
+          promptMessages.removeEmployee,
+          promptMessages.updateRole,
+          promptMessages.exit,
+        ]
+    })
+    .then(answer => {
+      console.log('answer', answer);
+      switch (answer.action) {
+        case promptMessages.viewAllEmployees:
+            viewAllEmployees();
+            break;
+    }})
+}
+
+function viewAllEmployees() {
+  const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+  FROM employee
+  LEFT JOIN employee manager on manager.id = employee.manager_id
+  INNER JOIN role ON (role.id = employee.role_id)
+  INNER JOIN department ON (department.id = role.department_id)
+  ORDER BY employee.id;`;
+    connection.query(query, (err, res) => {
+      if (err) throw err;
+      console.log('\n');
+      console.log('VIEW ALL EMPLOYEES');
+      console.log('\n');
+      console.table(res);
+      prompt();
+  });
+}
+
+prompt();
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
